a583645169ffb349902548eb3ad0064b3797df4f
dnrudzx <45260481+dnrudzx@users.noreply.github.com>
2020-10-23 21:45:28 +0900

    .

--- Library/내장/Math.py
+++ Library/내장/Math.py
@@ -1,5 +1,19 @@
 '''
-
+Math : 숫자 관련 라이브러리
 '''
 from math import *
 
+#print(ceil(1.1111))         #올림
+#print(floor(1.1111))        #내림
+#print(factorial(4))         #펙토리얼 반환
+'''쓸모 없어 보임'''
+#print(comb(1000,3))         #조합 / only 정수
+#print(copysign(-3.14,2))    #x의 절대값 / y의 부호
+#print(fabs(-30))            #x의 절대값 반환(float) - 근데 그냥 abs쓰셈
+
+
+
+
+
+
+print()
